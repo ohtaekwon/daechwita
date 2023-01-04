31f9cde8aa26bc92177544687a6a6c34903be34e
ohtaekwon <ohtk1119@gmail.com>
2023-01-04 21:48:32 +0900

    feat: section 컴포넌트 추상화 구현

---
+++ client/src/components/section/index.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import * as Styled from "./index.styles";
+import { Props } from "./index.types";
+import Flex from "../../_common/components/flex";
+import Grid from "../../_common/components/grid";
+
+const Section = ({
+  children,
+  sectionType,
+  variant,
+  width = 100,
+  direction = "row",
+  gridTemplateAreas = "",
+  gridArea = "",
+  paddingBottom = 0,
+  paddingLeft = 0,
+  paddingRight = 0,
+  paddingTop = 0,
+  marginTop = 0,
+  marginRight = 0,
+  marginBottom = 0,
+  marginLeft = 0,
+}: React.PropsWithChildren<Props>) => {
+  return (
+    <>
+      <Styled.Section
+        variant={variant}
+        width={width}
+        paddingBottom={paddingBottom}
+        paddingLeft={paddingLeft}
+        paddingRight={paddingRight}
+        paddingTop={paddingTop}
+        marginBottom={marginBottom}
+        marginLeft={marginLeft}
+        marginRight={marginRight}
+        marginTop={marginTop}
+        sectionType={sectionType}
+        direction={direction}
+        gridTemplateAreas={gridTemplateAreas}
+        gridArea={gridArea}
+      >
+        {sectionType == "flex" && <Flex direction={direction}>{children}</Flex>}
+        {sectionType == "grid" && (
+          <Grid gridTemplateAreas={gridTemplateAreas}>{children}</Grid>
+        )}
+        {sectionType == undefined && <div>{children}</div>}
+      </Styled.Section>
+    </>
+  );
+};
+export default Section;
