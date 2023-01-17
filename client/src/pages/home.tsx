5f5d299567a06e6d4418c8ae3f7401dfd1714c3d
ohtaekwon <ohtk1119@gmail.com>
2023-01-17 22:10:17 +0900

    feat: 임시 로그인 / 회원가입

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -2,6 +2,23 @@ import React from "react";
 import Layout from "../components/layout";
 
 const Home = () => {
-  return <Layout variant="default"></Layout>;
+  return (
+    <Layout variant="default">
+      <h3>회원가입</h3>
+      <input placeholder="Email.." />
+      <input placeholder="Password..." />
+      <button>회원가입</button>
+
+      <div>
+        <h3>Login</h3>
+        <input placeholder="Email" />
+        <input placeholder="Password" />
+        <button>Login</button>
+      </div>
+
+      <h4>logged In :</h4>
+      <button>로그아웃</button>
+    </Layout>
+  );
 };
 export default Home;
