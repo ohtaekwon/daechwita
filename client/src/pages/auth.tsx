ab3cc086ae1518677d491fe403bf84ce14d32751
ohtaekwon <ohtk1119@gmail.com>
2023-01-19 23:36:21 +0900

    feat: 임시 auth페이지 구현

--- client/src/pages/auth.tsx
+++ client/src/pages/auth.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
+import Button from "_common/components/button";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const onChange = (e: SyntheticEvent) => {
+    // console.log((e.target as HTMLInputElement).name);
+    const {
+      target: { name, value },
+    } = e as any;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <h1>Auth 페이지 입니다.</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="email"
+          value={email}
+          placeholder="Email.."
+          onChange={onChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          placeholder="Password..."
+          onChange={onChange}
+          required
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <Button variant={"primary"}>Continue with Google</Button>
+        <Button variant={"primary"}>Continue with Github</Button>
+      </div>
+    </>
+  );
 };
 
 export default Auth;
