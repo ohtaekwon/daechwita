598c3e5f342d13a716e5503933a957749ab53de0
ohtaekwon <ohtk1119@gmail.com>
2023-01-22 21:13:11 +0900

    feat: 임시 페이지

---
+++ client/src/pages/my-documents.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyDocuments = () => {
+  return <div>my-documents 페이지</div>;
+};
+
+export default MyDocuments;
--- client/src/pages/profile.tsx
+++ client/src/pages/profile.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return <div>Profile 페이지</div>;
 };
 
 export default Profile;
