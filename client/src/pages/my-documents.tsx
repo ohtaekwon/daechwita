38a2e530fb3752df82a83dd286261163c151d26f
ohtaekwon <ohtk1119@gmail.com>
2023-01-24 20:14:15 +0900

    chore: 텍스트 입력 폼

--- client/src/pages/my-documents.tsx
+++ client/src/pages/my-documents.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
+import Button from "_common/components/button";
 
 const MyDocuments = () => {
-  return <div>my-documents 페이지</div>;
+  const [document, setDocument] = React.useState<string>("");
+  const onSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+  };
+  const onChange = (e: SyntheticEvent) => {
+    const newDocs = (e.target as HTMLInputElement).value;
+    setDocument(newDocs);
+  };
+  console.log(document);
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        value={document}
+        onChange={onChange}
+        placeholder="글을 입력해주세요"
+        maxLength={120}
+      />
+      <Button
+        variant="zinc_200"
+        paddingY={9}
+        paddingX={16}
+        fontSize="md"
+        lineHeight="md"
+        color="zinc_400"
+      >
+        입력
+      </Button>
+    </form>
+  );
 };
 
 export default MyDocuments;
