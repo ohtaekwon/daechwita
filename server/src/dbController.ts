cbf28cb86d05db69e072ccebe8acb4653ced875e
ohtaekwon <ohtk1119@gmail.com>
2023-01-24 20:13:42 +0900

    feat: 임시 서버, dbcontroller 구현

---
+++ server/src/dbController.ts
@@ -0,0 +1,32 @@
+import fs, { fdatasync } from "fs";
+import { resolve } from "path";
+
+export enum DBField {
+  DOCUMENTS = "documents",
+  users = "users",
+}
+
+const basePath = resolve(); // 현재의 경로가 변수에 할당된다. == __dirname
+
+const filedNames = {
+  [DBField.DOCUMENTS]: resolve(basePath, "./src/db/documents.json"),
+  [DBField.users]: resolve(basePath, "./src/db/users.json"),
+};
+
+export const readDB = (target: DBField) => {
+  try {
+    return JSON.parse(
+      fs.readFileSync(filedNames[target], { encoding: "utf-8" })
+    );
+  } catch (err) {
+    console.log("read", err);
+  }
+};
+
+export const writeDB = (target: DBField, data: any) => {
+  try {
+    fs.writeFileSync(filedNames[target], JSON.stringify(data, null, "  "));
+  } catch (err) {
+    console.log("write", err);
+  }
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,14 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.listen({ port: 8000 });
-
 console.log("server is listening on port 8000...");
