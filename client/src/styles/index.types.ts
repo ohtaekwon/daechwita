d59291d9069b5d7b80dca5c57cb875584737c692
ohtaekwon <ohtk1119@gmail.com>
2023-02-03 21:58:08 +0900

    chore: 테마 타입

---
+++ client/src/styles/index.types.ts
@@ -0,0 +1,17 @@
+export type ThemeType = {
+  colors: {
+    [key: string]: string;
+  };
+  fontSize: {
+    [key: string]: string;
+  };
+  lineHeight: {
+    [key: string]: string;
+  };
+  breakpoint: {
+    [key: string]: string;
+  };
+  mediaQuery: {
+    [key: string]: string;
+  };
+};
