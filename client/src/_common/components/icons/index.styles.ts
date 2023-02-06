737f7e64a9febbbd294abb3799dc4c03efc6c977
ohtaekwon <ohtk1119@gmail.com>
2023-02-06 12:37:23 +0900

    style: div엘리먼트에서 span으로 변경

--- client/src/_common/components/icons/index.styles.ts
+++ client/src/_common/components/icons/index.styles.ts
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 import { IconsProps } from "./index.types";
 
-export const Icons = styled.div<Required<IconsProps>>`
+export const Icons = styled.span<Required<IconsProps>>`
   background-color: ${({ backgroundColor, theme }) =>
     theme.colors[backgroundColor]};
 `;
