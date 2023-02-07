f5d85b8950a54092635de3e0f18691631f8e2800
ohtaekwon <ohtk1119@gmail.com>
2023-02-07 18:37:10 +0900

    feat: vanlia와 react-beautiful-dnd 사용 칸반

---
+++ client/src/example/kanban/todoExample.tsx
@@ -0,0 +1,68 @@
+import { TItems, TItemStatus } from "pages/myDashBoard";
+import { Dispatch, SetStateAction, useEffect } from "react";
+import registDND from "./todoExample.drag";
+
+export default function TodoExample({
+  items,
+  setItems,
+}: {
+  items: TItems;
+  setItems: Dispatch<SetStateAction<TItems>>;
+}) {
+  useEffect(() => {
+    const clear = registDND(({ source, destination }) => {
+      if (!destination) return;
+
+      const scourceKey = source.droppableId as TItemStatus;
+      const destinationKey = destination.droppableId as TItemStatus;
+
+      setItems((items) => {
+        const _items = JSON.parse(JSON.stringify(items)) as typeof items;
+        const [targetItem] = _items[scourceKey].splice(source.index, 1);
+        _items[destinationKey].splice(destination.index, 0, targetItem);
+        return _items;
+      });
+    });
+    return () => clear();
+  }, [setItems]);
+
+  return (
+    <div className="p-4">
+      <div className="mb-2">
+        <h1 className="text-3xl font-bold">Drag and Drop</h1>
+        <span>React with VanillaTS (might have a little bug...)</span>
+      </div>
+
+      <div className="mt-4 flex" style={{ display: "flex" }}>
+        <div
+          className="todo grid flex-1 select-none grid-cols-2 gap-4 rounded-lg"
+          style={{ display: "flex" }}
+        >
+          {Object.keys(items).map((key) => (
+            <div
+              key={key}
+              data-droppable-id={key}
+              className="flex flex-col gap-3 rounded-xl bg-gray-200 p-4 ring-1 ring-gray-300 transition-shadow dark:bg-[#000000]"
+            >
+              <span className="text-xs font-semibold">
+                {key.toLocaleUpperCase()}
+              </span>
+              {items[key as TItemStatus].map((item, index) => (
+                <div
+                  key={item.id}
+                  data-index={index}
+                  className="dnd-item rounded-lg bg-white p-4 transition-shadow dark:bg-[#121212]"
+                >
+                  <h5 className="font-semibold">{item.title}</h5>
+                  <span className="text-sm text-gray-500">
+                    Make the world beatiful
+                  </span>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
