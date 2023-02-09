85c69cf61ff6ff0f0a014e3211b5b413cb3fe3bd
ohtaekwon <ohtk1119@gmail.com>
2023-02-09 15:17:46 +0900

    chore: autoResize 훅, drag 훅 연결

--- client/src/components/kanban/task.tsx
+++ client/src/components/kanban/task.tsx
@@ -4,9 +4,10 @@ import Button from "_common/components/button";
 import { TaskModel } from "./models";
 
 import { RiDeleteBin6Line } from "react-icons/ri";
-import Column from "./column";
-import { ColumnType } from "./enums";
 import Textarea from "_common/components/textarea";
+import useAutoHeightTextarea from "hooks/useAutoHeightTextarea";
+import useTaskDragAndDrop from "hooks/useTaskDragAndDrop";
+
 type TaskProps = {
   index: number;
   task: TaskModel;
@@ -19,17 +20,33 @@ const Task = ({
   onDelete: handleDelete,
   onUpdate: handleUpdate,
 }: TaskProps) => {
+  const { ref, isDragging } = useTaskDragAndDrop<HTMLElement>({
+    task,
+    index,
+  });
+
+  const {
+    textAreaRef,
+    lineHeight,
+    checkItemChangeHandler,
+    checkItemEnterHandler,
+  } = useAutoHeightTextarea();
+
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const newTitle = e.target.value;
     handleUpdate(task.id, { ...task, title: newTitle });
+    console.log(e.target.value.length);
+    checkItemChangeHandler(e);
   };
 
   const handleDeleteClick = () => {
     handleDelete(task.id);
   };
-
+  // lineHeight * 27 + 60
+  // console.log(lineHeight * 30);
   return (
     <Box
+      ref={ref}
       as="div"
       role="group"
       position="relative"
@@ -38,6 +55,7 @@ const Task = ({
       height="70px"
       backgroundColor={task.color}
       cursor="grab"
+      opacity={isDragging ? 0.5 : 1}
     >
       <Button
         aria-label="delete-task"
@@ -53,11 +71,13 @@ const Task = ({
       </Button>
       <Textarea
         width="100px"
-        height="30px"
+        height={lineHeight * 27 + 60}
         margin="auto"
         textAlign="center"
+        ref={textAreaRef}
         value={task.title}
         onChange={handleChange}
+        onKeyDown={() => checkItemEnterHandler}
       >
         {task.title}
       </Textarea>
