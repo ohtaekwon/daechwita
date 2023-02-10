50dd5166c7b004cb60e06cece1d462ea9f2a39ba
ohtaekwon <ohtk1119@gmail.com>
2023-02-10 15:14:48 +0900

    chore: 새로고침시 동기화가 유지될 수 있도록 eventLister 사용

--- client/src/hooks/useLocalStorage.ts
+++ client/src/hooks/useLocalStorage.ts
@@ -1,7 +1,9 @@
 import React from "react";
+import { useEventListener } from "usehooks-ts";
 
 function useLocalStorage<T>(key: string, initialValue: T) {
-  const [storedValue, setStoredValue] = React.useState<T>(() => {
+  const readValue = React.useCallback((): T => {
+    // "window is undefined" 에러 방지
     if (typeof window === "undefined") {
       return initialValue;
     }
@@ -9,34 +11,42 @@ function useLocalStorage<T>(key: string, initialValue: T) {
       const item = window.localStorage.getItem(key);
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
-      console.log(error);
+      console.warn(`Error reading localStorage key “${key}”:`, error);
       return initialValue;
     }
-  });
+  }, [initialValue, key]);
+
+  const [storedValue, setStoredValue] = React.useState<T>(readValue);
+
   const setValue = (value: T | ((val: T) => T)) => {
     try {
-      const localStorageData = window.localStorage.getItem(key);
-
-      if (!localStorageData) throw new Error("No saved localStorage data");
-
       const valueToStore =
-        value instanceof Function ? value(JSON.parse(localStorageData)) : value;
-      console.log("storevalue", storedValue);
-
-      console.log("valueToStore", valueToStore);
-
+        value instanceof Function ? value(storedValue) : value;
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
       setStoredValue(valueToStore);
-
-      console.log("stroedValue", storedValue);
-      if (typeof window !== "undefined") {
-        window.localStorage.setItem(key, JSON.stringify(valueToStore));
-        console.log("로컬에 저장후 상태", storedValue);
-      }
+      window.dispatchEvent(new Event("local-storage"));
     } catch (error) {
       console.log(error);
     }
-    console.log("STORE", storedValue);
   };
+
+  React.useEffect(() => {
+    setStoredValue(readValue());
+  }, []);
+
+  const handleStorageChange = React.useCallback(
+    (event: StorageEvent | CustomEvent) => {
+      if ((event as StorageEvent)?.key && (event as StorageEvent).key !== key) {
+        return;
+      }
+      setStoredValue(readValue());
+    },
+    [key, readValue]
+  );
+
+  useEventListener("storage", handleStorageChange);
+  useEventListener("local-storage", handleStorageChange);
+
   return [storedValue, setValue] as const;
 }
 export default useLocalStorage;
