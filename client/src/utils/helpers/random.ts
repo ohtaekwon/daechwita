5edb36ad92ca6e02fa6d66f30cbabd4ad451f5a2
ohtaekwon <ohtk1119@gmail.com>
2023-02-11 19:39:11 +0900

    chore: 색상 랜던 선택함수의 리턴값에 타입 지정

--- client/src/utils/helpers/random.ts
+++ client/src/utils/helpers/random.ts
@@ -1,7 +1,8 @@
+import { theme } from "styles";
 import { colors } from "utils/constants/color";
 
 function pickRandomColor(variant = "") {
   const color = colors[Math.floor(Math.random() * colors.length)];
-  return color + variant;
+  return (color + variant) as keyof typeof theme.colors;
 }
 export default pickRandomColor;
