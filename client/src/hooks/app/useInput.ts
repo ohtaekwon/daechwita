04372c260df924bd164cfe21edd975691182292e
ohtaekwon <ohtk1119@gmail.com>
2023-02-13 19:58:42 +0900

    feat: useInput 커스텀 훅

--- client/src/hooks/app/useInput.ts
+++ client/src/hooks/app/useInput.ts
@@ -1,3 +1,15 @@
 import React from "react";
 
-function useInput(initialValue: any) {}
+function useInput(initialValue: string) {
+  const [value, setValue] = React.useState(initialValue);
+
+  const handleChange = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(event.target.value);
+    },
+    []
+  );
+
+  return [value, handleChange, setValue] as const;
+}
+export default useInput;
