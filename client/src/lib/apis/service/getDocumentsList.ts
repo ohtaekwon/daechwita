7b4d4d9bb758d1624c7de1086a3350484db803c7
ohtaekwon <ohtk1119@gmail.com>
2023-02-15 18:10:11 +0900

    feat: documents service

---
+++ client/src/lib/apis/service/getDocumentsList.ts
@@ -0,0 +1,28 @@
+import { DocumentsType } from "types/index.types";
+
+// 데이터 정제
+export const getDocumentsList = (rowData: DocumentsType[]) => {
+  return rowData.map(
+    ({
+      id,
+      uid,
+      apply: { department, company },
+      text,
+      title,
+      tag,
+      createdAt: { seconds, nanoseconds },
+    }: DocumentsType) => {
+      return {
+        id,
+        uid,
+        department,
+        company,
+        tag,
+        text,
+        title,
+        seconds,
+        nanoseconds,
+      };
+    }
+  );
+};
