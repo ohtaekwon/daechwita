40af31eb3b4bd5b2fc2cb4ef81dc86513b12d4c1
ohtaekwon <ohtk1119@gmail.com>
2023-03-09 12:53:01 +0900

    chore: constant 파일로 통합

---
+++ client/src/utils/constants.ts
@@ -0,0 +1,30 @@
+/**
+ * URL
+ */
+export const BASE_URL = "localhost:3000";
+export const BASE_URL_APPLICATIONS = "/applications";
+export const BASE_URL_USERS = "/users";
+export const BASE_URL_SCHEDULES = "/applications";
+export const BASE_URL_RESUMES = "/resumes";
+/**
+ * 색상
+ */
+export const colors = [
+  "gray",
+  "stone",
+  "neutral",
+  "zinc",
+  "red",
+  "orange",
+  "amber",
+  "yellow",
+  "green",
+  "teal",
+  "sky",
+  "indigo",
+  "purple",
+];
+
+export const emoji = {
+  DOCUMENT: "📜",
+} as const;
