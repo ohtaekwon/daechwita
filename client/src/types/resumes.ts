7fc7744b8a7c729d2c6eb948a2966dc66af825f9
ohtaekwon <ohtk1119@gmail.com>
2023-03-22 23:04:01 +0900

    feat resumes type 정의

---
+++ client/src/types/resumes.ts
@@ -0,0 +1,22 @@
+import { TimeType } from "./schedule";
+
+export interface ResumesType {
+  id: string;
+  createdAt: TimeType;
+  uid: string;
+  imgUrl: string;
+  updatedAt: null | TimeType;
+  resumes: {
+    apply: {
+      company: string;
+      department: string;
+    };
+    documents: {
+      id: string;
+      tag: string;
+      text: string;
+      title: string;
+    }[];
+  };
+  tag: (string | undefined)[];
+}
